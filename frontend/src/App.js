f81b188b487971c64b8441ff220421a8182d697a
Chaarlay <findcharliemiddleton@gmail.com>
2024-06-20 11:27:11 -0400

    nav updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <Navbar />
       </header>
+      
     </div>
   );
 }
