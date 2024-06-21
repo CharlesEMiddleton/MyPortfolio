324279c5998a43e24c05f8153307e50148a2e850
Chaarlay <findcharliemiddleton@gmail.com>
2024-06-21 19:58:33 -0400

    fixing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import WeatherAPI from "../utilities/WeatherAPI";
+
 export default function Home (){
     
     return(
         <div>  
         <h1>Home</h1>
         <WeatherAPI />
-   
      </div>
 )
 }
