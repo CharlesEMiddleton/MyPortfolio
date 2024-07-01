9f49322df26fda0136ce71ea478260cc94de925d
Chaarlay <findcharliemiddleton@gmail.com>
2024-07-01 18:15:01 -0400

    improvements

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,14 +1,26 @@
 import React from "react";
 import WeatherAPI from "../../utilities/WeatherAPI/WeatherAPI";
 import JokeAPI from "../../utilities/JokeAPI/JokeAPI";
+import './Home.css'
 
 export default function Home (){
     
     return(
-        <div>  
+        <div className="baseContainer">  
+          
+      <div className="big-arrow-down"></div>
+     
+        <div className = "box1">
         <h1>Hello and welcome to my page</h1>
+        </div>
+        <div className = "box2" >
         <WeatherAPI />
+        </div>
+        
+        <div className = "box3">
         <JokeAPI />
+        </div>
+
      </div>
 )
 }
