738b97b05b517b0b5ffdb1b6882d35f4695b26bd
charlesemiddleton <findcharliemiddleton@gmail.com>
2024-07-01 19:23:39 -0700

    removing backend

--- backend/base/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import React
-
-class ReactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = React
-        fields = '__all__'
--- backend/base/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ReactView
-
-urlpatterns = [
-    path('ReactView/', ReactView.as_view(), name='ReactView'),
-]
